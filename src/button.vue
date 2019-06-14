8e94a5a4c4a26ddfe741d3fa37ba6bd1c48e53f0
xiaoFeng <804875728@qq.com>
2019-06-14 19:23:25 +0800

    icon组件

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,18 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button :class="{[`icon-${iconPosition}`]:true}" class="g-button">
+        <svg class="icon" v-if="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+
+    </button>
 </template>
 
 <script>
     export default {
-
+        props: ['icon', 'iconPosition']
     }
 </script>
 
@@ -16,6 +24,10 @@
         border-radius:var(--border-radius);
         border:1px solid var(--border-color);
         background:var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         &:hover{
             border-color:var(--border-color-hover);
         }
@@ -25,6 +37,30 @@
         &:focus{
             outline:none;
         }
+
+        > .content {
+            order: 2;
+        }
+
+        > .icon {
+            order: 1;
+            margin-right: .1em;
+        }
+
+        &.icon-right {
+            > .content {
+                order: 1;
+            }
+
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .1em
+            }
+
+        }
+
     }
+
 
 </style>
