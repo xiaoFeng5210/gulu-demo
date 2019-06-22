df1449f7c69834b8ab6abf6305e1b3fd4c871d17
xiaofeng <804875728@qq.com>
2019-06-22 10:11:41 +0800

    布局组件提交

---
+++ src/layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="layout">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "layout"
+    }
+</script>
+
+<style scoped lang="scss">
+.layout{
+
+}
+</style>
