4f213ac839a7766ef314065c61b0583f7caf3719
xiaofeng <804875728@qq.com>
2019-08-09 12:26:15 +0800

    组件基本完成

--- src/tabs-pane.vue
+++ src/tabs-pane.vue
@@ -3,39 +3,39 @@
         <slot></slot>
     </div>
 </template>
-
 <script>
     export default {
-        name: "tabs-pane",
-        inject:['eventBus'],
-        data(){
+        name: 'GuluTabsPane',
+        inject: ['eventBus'],
+        data () {
             return {
-                active:false
+                active: false
             }
         },
-        props:{
-            name:{
-                type:String,
-                required:true
+        props: {
+            name: {
+                type: String | Number,
+                required: true
             }
         },
-        computed:{
-            classes(){
+        computed: {
+            classes () {
                 return {
-                    active:this.active
+                    active: this.active
                 }
             }
         },
-        created(){
-            this.eventBus.$on('update:selected',(name)=>{
+        created () {
+            this.eventBus.$on('update:selected', (name) => {
                 this.active = name === this.name;
             })
         }
     }
 </script>
-
-<style scoped lang="scss">
-    .tabs-pane{
+<style lang="scss" scoped>
+    .tabs-pane {
+        padding: 1em;
+        &.active {
+        }
     }
-
 </style>
